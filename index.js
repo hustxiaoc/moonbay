fcd4874c7367caa7fd00727df8237152030aa3b6
淘杰 <taojie.hjp@taobao.com>
2016-12-24 22:23:50 +0800

    refactor: update config

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const isLocal = process.env.NODE_ENV !== 'production' && !process.env.EGG_SERVER
 require('egg').startCluster({
   baseDir: __dirname,
   port: process.env.LEANCLOUD_APP_PORT || 7001, // default to 7001
-  workers: isLocal ? 1 : undefined, // default to cpu count
+  workers: isLocal ? 1 : 1, // default to cpu count
 });
